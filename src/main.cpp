86691cc7e2efda3ba03682c583680b137063e9b9
aseluk-official <ahmetselim072@gmail.com>
2026-06-20 14:50:32 +0300

    Added permission check & root owner check. Other than that now program shows all of the files that are created by the package when you use delete command also made the command buffer size 4096

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,11 @@
 #include <string>
 #include <filesystem>
 #include <algorithm>
+#include <sys/stat.h>
+#include <unistd.h>
 
 std::string runCommand(const std::string& cmd) {
-    std::array<char, 456> buffer;
+    std::array<char, 4096> buffer;
     std::string result;
 
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
@@ -30,6 +32,21 @@ std::string runCommandAsAdmin(const std::string& cmd){
     std::replace(result.begin(), result.end(), '\r', '\n');
 
     return result;
+}
+
+bool isRootOwned(const std::string& path)
+{
+    struct stat info;
+    if (stat(path.c_str(), &info) == 0)
+    {
+        return !info.st_uid;
+    }
+    throw std::runtime_error("File or directory does not exist: " + path);
+}
+
+bool canWriteAndModify(const std::string &path)
+{
+    return (access(path.c_str(), W_OK) == 0);
 }
 
 const std::string helpText = "Help text";
@@ -60,7 +77,9 @@ int main(){
         }
         else if (command.find("delete ") == 0){
             command.erase(0, 7); // delete the "delete " from the text
-            std::cout << command << "\n";
+            std::string filesCreated = runCommand("pkgutil --files " + command);
+            std::cout << filesCreated << "\n";
+            //runCommandAsAdmin("pkgutil --forget " + command); NOT DOING IT NOW I DON'T WANNA SCREW MYSELF FOR NOW
         }
         else {
             std::cout << "Type \"help\" for help\n";
